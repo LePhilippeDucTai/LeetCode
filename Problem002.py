d286cf11047882b4bd9a24a0beff98d59c0dc5f6
Philippe LÊ <philippele@iMac-de-Philippe.local>
2019-12-16 16:16:13 +0100

    Initial commit. Made the second problem of LeetCode.

---
+++ Problem002.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __repr__(self):
+        if(self.next == None) :
+            return(str(self.val))
+        else :
+            return(str(self.val) + ' -> ' + str(self.next))
+
+def toNum(l : ListNode) -> int:
+    def loop(acc : int, lp : ListNode, multp : int) -> int :
+        if not lp :
+            return(acc)
+        else :
+            return(loop(acc + lp.val * multp, lp.next, multp * 10))
+    return(loop(acc = 0, lp = l, multp = 1))
+
+def listToNode(l : list) -> ListNode:
+    x, *xs = l
+    L_ret = ListNode(x)
+    if not xs:
+        return(L_ret)
+    else :
+        L_ret.next = listToNode(xs)
+    return(L_ret)
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        x = toNum(l1) + toNum(l2)
+        string_x_rev = reversed(str(x))
+        toNode = lambda y : listToNode(map(int, string_x_rev))
+        return(toNode(x))
+
