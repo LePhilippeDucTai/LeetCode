3d4e54a6a0b04135b390e6d8fc860fc1e0c57519
LePhilippeDucTai <le.philippe.duc.tai@gmail.com>
2020-03-16 22:20:57 +0100

    More efficient computation of the maximum area. Added a recursive function, and a while. Recursive avoids state variables.

--- Problem011.py
+++ Problem011.py
@@ -1,22 +1,33 @@
 from typing import List
+import functools as ft
 class Solution:
-    def area(self, u,v):
-        x1, y1 = u
-        x2, y2 = v
-        return abs(x1-x2) * min(y1,y2)
-
     def maxArea(self, height: List[int]) -> int:
-        n = len(height)
-        maximum = -1
-        for i in range(n):
-            for j in range(i + 1, n):
-                maximum = max(maximum, (j - i)*min(height[i], height[j]))
-        return maximum
+        def loop(i, j, acc) :
+            distance = j - i
+            if distance :
+                if height[i] < height[j] :
+                    return loop(i + 1, j, max(acc, distance * height[i]))
+                else :
+                    return loop(i, j - 1, max(acc, distance * height[j]))
+            else :
+                return acc
+        return loop(0, len(height) - 1, 0)
 
+    def maxArea2(self, height: List[int]) -> int:
+        maximum = -1
+        i, j = 0, len(height) - 1
+        while i < j :
+            if height[i] < height[j] :
+                maximum = max(maximum, (j - i) * height[i])
+                i += 1
+            else :
+                maximum = max(maximum, (j-i) * height[j])
+                j -= 1
+        return maximum
 
 if __name__ == "__main__":
     s = Solution()
     heights = [1,8,6,2,5,4,8,3,7]
+    couples = enumerate(heights)
     r = s.maxArea(heights)
     print(r)
-    
