732d142da3dec2df730409d90e7a80875d0fd5d8
LePhilippeDucTai <le.philippe.duc.tai@gmail.com>
2020-03-20 12:20:48 +0100

    Problem027 solved.

---
+++ Problem027.py
@@ -0,0 +1,15 @@
+from typing import List
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        try:
+            while True:
+                nums.remove(val)
+        except:
+            return len(nums)
+
+if __name__ == "__main__":
+    S = Solution()
+    nums = [3,2,2,3]
+    x = S.removeElement(nums, 3)
+    print(x, nums)
+    
